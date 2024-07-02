5657d8c7f2c7adde811bd3ae2170199c017fa84c
Luk <Lukasz.jablonski1991@gmail.com>
2024-07-02 15:36:49 +0200

    auth guard moved to auth folder

--- src/app/auth/_guards/auth.guard.ts
+++ src/app/auth/_guards/auth.guard.ts
@@ -1,16 +1,31 @@
 import { inject } from '@angular/core';
 import { CanMatchFn, Router } from '@angular/router';
+<<<<<<< HEAD
 import { AuthService } from '../auth.service';
 import { Roles } from '../../shared/_enums/roles';
+=======
+import { Observable } from 'rxjs';
+import { map, take } from 'rxjs/operators';
+import { AuthService } from '../auth.service';
+>>>>>>> 2ca65a2 (auth guard moved to auth folder)
 
 export const authGuard: CanMatchFn = (): boolean => {
   const authService = inject(AuthService);
   const router = inject(Router);
+<<<<<<< HEAD
 
   if (authService.user$()?.roles?.includes(Roles.ADMIN)) {
+=======
+  console.log('GUARD', authService.isAdmin$());
+  if (authService.isAdmin$()) {
+>>>>>>> 2ca65a2 (auth guard moved to auth folder)
     return true;
   } else {
     router.navigate(['/posts']);
     return false;
   }
+<<<<<<< HEAD
+=======
+
+>>>>>>> 2ca65a2 (auth guard moved to auth folder)
 };
