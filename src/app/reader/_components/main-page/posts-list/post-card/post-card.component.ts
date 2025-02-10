fcacc9e39fdd4479895d1d740450fa209fb6970b
Luk <Lukasz.jablonski1991@gmail.com>
2025-02-10 20:54:54 +0100

    added new bg, decription to card. todo: tags inthe db,  link to post, post,

--- src/app/reader/_components/main-page/posts-list/post-card/post-card.component.ts
+++ src/app/reader/_components/main-page/posts-list/post-card/post-card.component.ts
@@ -1,30 +1,21 @@
-import {Component, computed, inject, input} from '@angular/core';
-import {Post} from "../../../../../shared/_models/post.interface";
-import {DatePipe} from "@angular/common";
-import {RouterLink} from "@angular/router";
-import {LabelComponent} from "../label/label.component";
-import {DomSanitizer} from "@angular/platform-browser";
-import {TAGS} from "../../../../../utlis/tags";
+import { Component, computed, input } from '@angular/core';
+import { Post } from '../../../../../shared/_models/post.interface';
+import { LabelComponent } from '../label/label.component';
+import { TAGS } from '../../../../../utlis/tags';
 
 @Component({
   selector: 'app-post-card',
   standalone: true,
-  imports: [
-    DatePipe,
-    RouterLink,
-    LabelComponent
-  ],
+  imports: [LabelComponent],
   templateUrl: './post-card.component.html',
-  styleUrl: './post-card.component.scss'
+  styleUrl: './post-card.component.scss',
 })
 export class PostCardComponent {
-  tagsMock = ['HTML', 'CSS', 'TypeScript', 'SSG/SSR']
-  tags = TAGS.filter(tag => this.tagsMock.includes(tag.tag));
-  sanitizer = inject(DomSanitizer);
+  tagsMock = ['HTML', 'CSS', 'TypeScript', 'SSG/SSR'];
+  tags = TAGS.filter((tag) => this.tagsMock.includes(tag.tag));
 
   post = input.required<Post>();
-  sanitizedContent = computed(() => {
-    return this.sanitizer.bypassSecurityTrustHtml(this.post().content.toString().substring(0, 1000));
-  })
-
+  description = computed(() => {
+    return this.post().description;
+  });
 }
