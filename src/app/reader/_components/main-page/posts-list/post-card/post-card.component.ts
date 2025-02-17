bc80f785e2c20b7ea803547029fc6eda330c791a
Luk <Lukasz.jablonski1991@gmail.com>
2025-02-17 18:29:16 +0100

    front page

--- src/app/reader/_components/main-page/posts-list/post-card/post-card.component.ts
+++ src/app/reader/_components/main-page/posts-list/post-card/post-card.component.ts
@@ -2,11 +2,12 @@ import { Component, computed, input } from '@angular/core';
 import { Post } from '../../../../../shared/_models/post.interface';
 import { LabelComponent } from '../label/label.component';
 import { TAGS } from '../../../../../utlis/tags';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-post-card',
   standalone: true,
-  imports: [LabelComponent],
+  imports: [LabelComponent, RouterLink],
   templateUrl: './post-card.component.html',
   styleUrl: './post-card.component.scss',
 })
