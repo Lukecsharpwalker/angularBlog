6a2aa9405bd388072f2d4abaa6f7e00674aff15a
Luk <Lukasz.jablonski1991@gmail.com>
2025-05-01 11:31:55 +0200

    fix for build

--- src/app/auth/_guards/auth.guard.spec.ts
+++ src/app/auth/_guards/auth.guard.spec.ts
@@ -1,11 +1,10 @@
 import { TestBed } from '@angular/core/testing';
 import { CanMatchFn } from '@angular/router';
-
-import { authGuard } from './auth.guard';
+import { authAdminGuard } from './authAdminGuard';
 
 describe('authGuard', () => {
-  const executeGuard: CanMatchFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => authGuard(...guardParameters));
+  const executeGuard: CanMatchFn = (...guardParameters) =>
+    TestBed.runInInjectionContext(() => authAdminGuard(...guardParameters));
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
