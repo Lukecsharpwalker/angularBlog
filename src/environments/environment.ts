abb5351a7018d5ee24df799e391c67e6f27dc8ab
Luk <Lukasz.jablonski1991@gmail.com>
2025-05-02 16:50:32 +0200

    envs added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  supabaseUrl: '',
-  supabaseKey: '',
+  supabaseUrl: 'https://aqdbdmepncxxuanlymwr.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFxZGJkbWVwbmN4eHVhbmx5bXdyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUwNTA0MjYsImV4cCI6MjA2MDYyNjQyNn0.RNtZZ4Of4LIP3XuS9lumHYdjRLVUGXARtAxaTJmF7lc',
 };
