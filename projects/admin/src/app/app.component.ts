f9a2b095fec3f5cf3bcf6e377c842c20de373f07
Luk <Lukasz.jablonski1991@gmail.com>
2025-08-18 18:37:45 +0200

    Refactor component selectors and add ESLint configuration for multiple projects

--- projects/admin/src/app/app.component.ts
+++ projects/admin/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'admin-root',
   imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
--- projects/web/src/app/app.component.ts
+++ projects/web/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'web-root',
   imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
