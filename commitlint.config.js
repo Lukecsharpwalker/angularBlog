cf256f356a35b35106ef637af48f8f507244af41
Luk <Lukasz.jablonski1991@gmail.com>
2025-08-27 19:05:37 +0200

    fix(config): update commitlint and eslint configurations for improved rule definitions

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,28 +1,12 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'scope-enum': [
-      2,
-      'always',
-      ['web', 'admin', 'mfe', 'shared', 'config']
-    ],
+    'scope-enum': [2, 'always', ['web', 'admin', 'mfe', 'shared', 'config']],
     'scope-empty': [2, 'never'],
-    'subject-case': [2, 'always', 'lower-case'],
     'type-enum': [
       2,
       'always',
-      [
-        'feat',
-        'fix',
-        'docs',
-        'style',
-        'refactor',
-        'test',
-        'build',
-        'ci',
-        'chore',
-        'revert'
-      ]
-    ]
-  }
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'build', 'ci', 'chore', 'revert'],
+    ],
+  },
 };
