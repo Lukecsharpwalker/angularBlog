770887ccd6af00bbfe58fbfddea36b291903daa6
Lukasz Jablonski <34306549+Lukecsharpwalker@users.noreply.github.com>
2026-09-05 12:09:14 +0200

    chore(shared): trash

--- projects/admin/src/app/features/add-post/add-post.constants.ts
+++ projects/admin/src/app/features/add-post/add-post.constants.ts
@@ -2,11 +2,3 @@ export const ADD_POST_CONSTANTS = {
   DESCRIPTION_MAX_LENGTH: 450,
   IMAGE_MAX_WIDTH_STYLE: 'max-width: 100%;',
 };
-
-export const MODAL_CONFIG_DEFAULTS = {
-  ADD_IMAGE: {
-    title: 'Add Image',
-    primaryButton: 'Insert',
-    secondaryButton: 'Cancel',
-  }
-};
--- projects/web/src/app/app.routes.ts
+++ projects/web/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
+    //TODO: https://github.com/Lukecsharpwalker/angularBlog/issues/123
     loadChildren: () => import('./features/main-page/main-page.routes'),
   },
   {
