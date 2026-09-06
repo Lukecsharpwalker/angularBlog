8d48635ed52b99a3e1a4812f21db5d67bb682ee2
Lukasz Jablonski <34306549+Lukecsharpwalker@users.noreply.github.com>
2026-09-06 21:08:16 +0200

    feat(web): add variant option to cover image preview dialog

--- projects/admin/src/app/features/add-post/add-post.component.ts
+++ projects/admin/src/app/features/add-post/add-post.component.ts
@@ -96,6 +96,7 @@ export class AddPostComponent implements OnInit {
   protected openCoverImagePreview(coverImage: string): void {
     this.dynamicDialogService.openDialog(this.viewContainerRef, {
       title: 'Cover image',
+      variant: 'casual',
       image: coverImage,
     });
   }
