8c1b3868858ace0711682b6489f54b411aa6c702
Lukasz Jablonski <34306549+Lukecsharpwalker@users.noreply.github.com>
2026-09-06 21:54:31 +0200

    refactor(shared): rename auth-form interface to auth-form model

--- projects/shared/src/pattern/auth-form/public-api.ts
+++ projects/shared/src/pattern/auth-form/public-api.ts
@@ -1,4 +1,4 @@
 export * from './auth-form.component';
 export * from './auth-form.service';
-export * from './auth-form.interface';
+export * from './auth-form.model';
 export * from './default-login-config';
