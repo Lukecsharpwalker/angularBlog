f06827c6451078b2e63a35167b5588bcff4fa5a2
Lukasz Jablonski <34306549+Lukecsharpwalker@users.noreply.github.com>
2026-09-06 16:23:07 +0200

    feat(web): improve layout and styling of modal and cookie consent components

--- projects/web/src/app/layout/main-layout/main-layout.component.ts
+++ projects/web/src/app/layout/main-layout/main-layout.component.ts
@@ -1,21 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { BottomNavComponent } from '../bottom-nav/bottom-nav.component';
 
 @Component({
   selector: 'web-main-layout',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, BottomNavComponent],
   template: `
-    <div
-      class="min-h-screen relative"
-    >
+    <div class="min-h-screen relative">
       <div class="fixed inset-0 pointer-events-none"></div>
 
-      <web-navbar class="relative z-50" />
-      <main class="container mx-auto w-11/12 lg:w-10/12 xl:w-8/12 py-8 relative z-10">
+      <web-navbar class="relative z-10" />
+      <main class="container relative mx-auto w-11/12 pt-8 pb-32 md:pb-8 lg:w-10/12 xl:w-8/12">
         <router-outlet />
       </main>
+
+      <web-bottom-nav />
     </div>
   `,
 })
